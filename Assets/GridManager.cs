9d50ed7f8510dd9b4e7f52cd65fef9ff5ca45a38
Harmitage <andrey.nsko@gmail.com>
2015-02-18 15:45:02 +0500

    Initial commit

---
+++ Assets/GridManager.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridManager: MonoBehaviour
+{
+	//following public variable is used to store the hex model prefab;
+	//instantiate it by dragging the prefab on this variable using unity editor
+	public GameObject Square;
+	public GameObject Ship;
+	//next two variables can also be instantiated using unity editor
+	public int gridWidthInSquares = 13;
+	public int gridHeightInSquares = 13;
+	
+	//Hexagon tile width and height in game world
+	private float squareWidth;
+	private float squareHeight;
+	
+	//Method to initialise Hexagon width and height
+	void setSizes()
+	{
+		//renderer component attached to the Hex prefab is used to get the current width and height
+		squareWidth = Square.renderer.bounds.size.x;
+		squareHeight = Square.renderer.bounds.size.z;
+	}
+	
+	//Method to calculate the position of the first hexagon tile
+	//The center of the hex grid is (0,0,0)
+	Vector3 calcInitPos()
+	{
+		Vector3 initPos;
+		//the initial position will be in the left upper corner
+		initPos = new Vector3(-squareWidth * gridWidthInSquares / 2f + squareWidth / 2, 0,
+		                      gridHeightInSquares / 2f * squareHeight - squareHeight / 2);
+		
+		return initPos;
+	}
+	
+	//method used to convert hex grid coordinates to game world coordinates
+	public Vector3 calcWorldCoord(Vector2 gridPos)
+	{
+		//Position of the first hex tile
+		Vector3 initPos = calcInitPos();
+		//Every second row is offset by half of the tile width
+		float offset = 0;
+		//if (gridPos.y % 2 != 0)
+		//	offset = squareWidth / 2;
+		
+		float x =  initPos.x + offset + gridPos.x * squareWidth;
+		//Every new line is offset in z direction by 3/4 of the hexagon height
+		float z = initPos.z - gridPos.y * squareHeight;
+		return new Vector3(x, 0, z);
+	}
+	
+	//Finally the method which initialises and positions all the tiles
+	void createGrid()
+	{
+		//Game object which is the parent of all the hex tiles
+		GameObject squareGridGO = new GameObject("SquareGrid");
+		
+		for (float y = 0; y < gridHeightInSquares; y++)
+		{
+			for (float x = 0; x < gridWidthInSquares; x++)
+			{
+				//GameObject assigned to Hex public variable is cloned
+				makeSquare(x,y,squareGridGO);
+			}
+		}
+		makeSquare(gridWidthInSquares/2, -1, squareGridGO);
+		makeSquare(gridWidthInSquares/2, gridHeightInSquares, squareGridGO);
+		makeSquare(-1, gridHeightInSquares/2, squareGridGO);
+		makeSquare(gridWidthInSquares, gridHeightInSquares/2, squareGridGO);
+	}
+
+	//Setup starting squares
+	void makeSquare(float x,float y,GameObject squareGridGO)
+	{
+		GameObject square = (GameObject)Instantiate(Square);
+		//Current position in grid
+		Vector2 gridPos = new Vector2(x, y);
+		square.transform.position = calcWorldCoord(gridPos);
+		square.transform.parent = squareGridGO.transform;
+	}
+
+	//Place ships
+	void placeShips(float x, float y)
+	{
+		GameObject ship = (GameObject)Instantiate(Ship);
+	}
+	
+	//The grid should be generated on game start
+	void Start()
+	{
+		setSizes();
+		createGrid();
+	}
+}
